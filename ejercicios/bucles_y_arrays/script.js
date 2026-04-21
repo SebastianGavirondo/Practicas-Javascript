fd30eb32d5041e825942b178b600b3db21e0c79a
Sebastian Gavirondo <sebaa214364@gmail.com>
2026-04-20 22:26:10 -0300

    Bucles y arrays. Ejercicios terminados del 1 al 3

---
+++ ejercicios/bucles_y_arrays/script.js
@@ -0,0 +1,8 @@
+let suma = 0;
+
+for (let contador = 0; suma < 10; contador++) {
+  suma = suma + 2;
+  console.log(suma);
+}
+
+console.log("Suma total: ", suma);
---
+++ ejercicios/bucles_y_arrays/script3.js
@@ -0,0 +1,11 @@
+const numeros = [4, 17, 3, 25, 8, 11];
+let mayor = numeros[0];
+
+for (let contador = 0; contador < numeros.length; contador++) {
+  if (numeros[contador] > mayor) {
+    mayor = numeros[contador];
+  }
+  console.log(numeros.lenght);
+}
+
+console.log("El número mayor es: ", mayor);
